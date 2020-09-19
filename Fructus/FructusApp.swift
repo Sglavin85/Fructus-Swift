bf948094466dca2d6ad469564d5af0e0df62ba3d
Sean Glavin <sean.glavin@hahparking.com>
2020-09-18 20:48:52 -0500

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Sean Glavin on 9/18/20.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
